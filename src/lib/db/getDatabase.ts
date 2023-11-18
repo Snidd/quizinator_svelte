b7b1644d36a1367539240a5ca9aa90c6ed7bda6c
Magnus Kjellberg <magnus.kjellberg@istone.com>
2023-11-18 21:07:50 +0100

    Testing singleton

--- src/lib/db/getDatabase.ts
+++ src/lib/db/getDatabase.ts
@@ -1,11 +1,18 @@
 import { env } from '$env/dynamic/private';
 import postgres from 'postgres';
 
+let globalDatabase: postgres.Sql<{}> | undefined = undefined;
+
 export function getDatabase() {
+
+	if (globalDatabase !== undefined) {
+		return globalDatabase;
+	}
+
 	const dbUrl = env.DATABASE_URL;
 	if (dbUrl === undefined) throw new Error('Unable to connect to database!');
 
-	const sql = postgres(dbUrl);
+	globalDatabase = postgres(dbUrl);
 
-	return sql;
+	return globalDatabase;
 }
